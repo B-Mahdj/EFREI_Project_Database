ea0d272940b7e6b2c4d0ef5ccc9246a04ec24d7d
Bilal Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2022-05-22 22:19:04 +0200

    Adding all the basic calls to database for all tables

--- backend/controllers/transactionControllers.js
+++ backend/controllers/transactionControllers.js
@@ -12,3 +12,28 @@ exports.getAllTransaction = (req, res, next) => {
         }
     });
 }
+
+exports.createTransaction = (req, res, next) => {
+    commission = req.query.commission;
+    prix_vente = req.query.prix_vente;
+    date_transac = req.query.date_transac;
+    id_client = req.query.id_client;
+    id_proprietaire = req.query.id_proprietaire;
+    if ( commission && prix_vente && date_transac && id_client && id_proprietaire) {
+        mysql.query('INSERT INTO Transaction VALUES (?, ?, ?, ?, ?)', [commission, prix_vente, date_transac, id_client, id_proprietaire], (err, result) => {
+            if (!err) {
+                res.status(200).json({
+                    message: 'Transaction créée !'
+                });
+            }
+            else {
+                res.status(500).json(err);
+            }
+        });
+    }
+    else {
+        res.status(500).json({
+            message: 'Veuillez remplir tous les champs !'
+        });
+    }
+}
--- backend/routes/adresseRoutes.js
+++ backend/routes/adresseRoutes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const adresseCtrl = require('../controllers/adresseControllers');
 
 router.get('/', adresseCtrl.getAllAdresse);
+router.post('/', adresseCtrl.createAdresse);
+router.get('/id', adresseCtrl.getAdresseById);
+router.put('/id', adresseCtrl.updateAdresse);
+router.delete('/id', adresseCtrl.deleteAdresse);
 
 module.exports = router;
