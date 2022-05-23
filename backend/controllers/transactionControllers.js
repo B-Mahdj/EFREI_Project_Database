c9ccf8ec985f8da2163441b67927debf3afed479
Bilal Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2022-05-23 14:53:51 +0200

    Adding more request

--- backend/controllers/transactionControllers.js
+++ backend/controllers/transactionControllers.js
@@ -37,3 +37,56 @@ exports.createTransaction = (req, res, next) => {
         });
     }
 }
+
+exports.getCountFromTransaction = (req, res, next) => {
+    mysql.query('SELECT COUNT(*) FROM Transaction', (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json(rows);
+            console.log(rows);
+        }
+        else {
+            res.status(500).json(err);
+            console.log(err);
+        }
+    });
+}
+
+
+exports.getSumPriceFromTransaction = (req, res, next) => {
+    mysql.query('SELECT SUM(prix_vente) FROM Transaction', (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json(rows);
+            console.log(rows);
+        }
+        else {
+            res.status(500).json(err);
+            console.log(err);
+        }
+    });
+}
+
+exports.getSumOfCommission = (req, res, next) => {
+    mysql.query('SELECT SUM(commission) FROM Transaction', (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json(rows);
+            console.log(rows);
+        }
+        else {
+            res.status(500).json(err);
+            console.log(err);
+        }
+    });
+}
+
+exports.getDateLastTransaction = (req, res, next) => {
+    mysql.query('SELECT date_transac FROM Transaction ORDER BY date_transac DESC LIMIT 1', (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json(rows);
+            console.log(rows);
+        }
+        else {
+            res.status(500).json(err);
+            console.log(err);
+        }
+    });
+}
--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -5,5 +5,9 @@ const transactionCtrl = require('../controllers/transactionControllers');
 
 router.get('/', transactionCtrl.getAllTransaction);
 router.post('/', transactionCtrl.createTransaction);
+router.get('/count', transactionCtrl.getCountFromTransaction);
+router.get('/sumPrice', transactionCtrl.getSumPriceFromTransaction);
+router.get('/sumCommission', transactionCtrl.getSumOfCommission);
+router.get('/derniere_vente', transactionCtrl.getDateLastTransaction);
 
 module.exports = router;
